c5a8b894acabc20b34e7c1c24e7db85d537753f4
Wenxuan Zhang <zhangwxxx@126.com>
2021-10-08 17:23:37 +0800

    debug1008

--- dataloaders/tiered_imagenet.py
+++ dataloaders/tiered_imagenet.py
@@ -65,7 +65,7 @@ def init_dataloaders(args):
     cl_ood_dataset2 = NonEpisodicTieredImagenet(args.folder, split="val").data
     args.num_domains = len(cl_dataset.keys())
     for k in cl_ood_dataset2.keys():
-        cl_ood_dataset2[k+args.num_domain] = cl_ood_dataset2.pop(k)
+        cl_ood_dataset2[k+args.num_domains] = cl_ood_dataset2.pop(k)
     args.num_domains += len(cl_ood_dataset2)
 
     cl_ood_dataset1 = cl_ood_dataset1
